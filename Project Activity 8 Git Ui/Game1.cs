bd782af5633c50e7a11242901b267ee3449ff1be
Patiphan_Activity_8 <Patiphan@LT-UDVJTST421D0>
2024-09-03 15:44:40 +0700

    update feature1

--- Project Activity 8 Git Ui/Game1.cs
+++ Project Activity 8 Git Ui/Game1.cs
@@ -8,6 +8,7 @@ namespace Project_Activity_8_Git_Ui
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private int num1;
 
         public Game1()
         {
